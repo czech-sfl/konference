f4e39d2ad55ab09a6075e3d3ccf6f73116be4a3c
Pokrt <honza.kadlec.cb@gmail.com>
2026-02-09 20:57:42 +0200

    video bg

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,17 @@
 <template>
   <Navbar />
-  <div class="hero min-h-screen" style="background-image: url(/img_2.jpg);">
+  <div class="hero min-h-screen relative overflow-hidden">
     <div class="hero-overlay"></div>
-    <div class="hero-content text-center text-neutral-content">
+    <video
+      autoplay
+      muted
+      loop
+      playsinline
+      class="absolute inset-0 w-full h-full object-cover"
+    >
+      <source src="/clean_video.mp4" type="video/mp4" />
+    </video>
+    <div class="hero-content text-center text-neutral-content relative z-10">
       <div class="mt-10">
         <div class="items-center w-full my-4">
           <h1 class="hidden lg:block font-burra text-4xl xl:text-9xl w-full font-bold text-secondary">
